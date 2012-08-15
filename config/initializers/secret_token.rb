24c301d3512e7642636deca55dc531a5317a7b77
Valeriy <valloskut@gmail.com>
2012-08-15 20:23:29 +0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Parser::Application.config.secret_token = 'be31f2e274d5a4ad3d8e5d7ada1c064d1a5048bca92366c4bc727faf7ee0f113767c71d49b9b92730ab1111b103e35c5eb433125d42e9aecba683002bbdbe109'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Parser::Application.config.session_store :cookie_store, key: '_parser_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Parser::Application.config.session_store :active_record_store
