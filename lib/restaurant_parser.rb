c1ca064225826544c79e3b74bd4d18e32ebf4e68
Valeriy <valloskut@gmail.com>
2012-08-18 14:29:58 +0600

    refactor methods for class variables in RestaurantParser

--- lib/restaurant_parser.rb
+++ lib/restaurant_parser.rb
@@ -1,114 +1,87 @@
 class RestaurantParser
-  extend ActiveSupport::Memoizable
+  attr_reader :fields
 
   def initialize(item, base)
     @item, @base = item, base
+    @name = @item.at_css("h3 a")
+    @actions = @item.at_css("ul.listing_summary_share_actions")
+    @address = @item.at_css("p.listing_summary_body_content_address")
+    @review = @item.at_css("div.listing_summary_body_content_review")
+    @first_review = @review.at_css("div.listing_summary_first_review a")
+    @reviewer_location = @review.at_css("span.reviewer_location").remove.at_css("a") unless @first_review
     @fields = {}
   end
 
-  def name(item)
-    item.at_css("h3 a").text
+  def name
+    @fields[:name] = @name.text
   end
 
-  def link(item)
-    item.at_css("h3 a")['href']
+  def link
+    @fields[:link] = @name['href']
   end
 
-  def twitter_link(item)
-    item.at_css("a.listing_summary_icon_twitter")["href"]
+  def twitter_link
+    @fields[:twitter_link] = @item.at_css("a.listing_summary_icon_twitter")["href"]
   end
 
-  def facebook_link(item)
-    item.at_css("a.listing_summary_icon_facebook")["href"]
+  def facebook_link
+    @fields[:facebook_link] = @item.at_css("a.listing_summary_icon_facebook")["href"]
   end
 
-  def map_link(item)
-    item.at_css("ul.listing_summary_share_actions li:nth-child(1) a")["href"]
+  def map_link
+    @fields[:map_link] = @actions.at_css("li:nth-child(1) a")["href"]
   end
 
-  def new_review_link(item)
-    item.at_css("ul.listing_summary_share_actions li:nth-child(2) a")["href"]
+  def new_review_link
+    @fields[:new_review_link] = @actions.at_css("li:nth-child(2) a")["href"]
   end
 
-  def email_link(item)
-    item.at_css("ul.listing_summary_share_actions li:nth-child(3) a")["href"]
+  def email_link
+    @fields[:email_link] = @actions.at_css("li:nth-child(3) a")["href"]
   end
 
-  def zip(item)
-    item.at_css("p.listing_summary_body_content_address span.zip").text
+  def zip
+    @fields[:zip] = @address.at_css("span.zip").remove.text
   end
 
-  def address(item)
-    address = item.at_css("p.listing_summary_body_content_address")
-    address.at_css("span.zip").remove
-    address.text.split(/\s+/).reject(&:empty?).join(" ")
+  def address
+    zip unless @fields[:zip]
+    @fields[:address] = @address.text.split(/\s+/).reject(&:empty?).join(" ")
   end
 
-  def image_link(item)
-    item.at_css("img.listing_summary_image")['src']
+  def image_link
+    @fields[:image_link] = @item.at_css("img.listing_summary_image")['src']
   end
 
-  def first_review_link(item)
-    item.at_css("div.listing_summary_body_content_review div.listing_summary_first_review a")["href"]
+  def first_review_link
+    @fields[:first_review_link] = @first_review["href"] if @first_review
   end
 
-  memoize :first_review_link
-
-  def reviewer_location(item)
-    unless first_review_link(item)
-      item.at_css("div.listing_summary_body_content_review span.reviewer_location a").text
-    end
+  def reviewer_location
+    @fields[:reviewer_location] = @reviewer_location.text unless @first_review
   end
 
-  def reviewer_location_link(item)
-    unless first_review_link(item)
-      item.at_css("div.listing_summary_body_content_review span.reviewer_location a")["href"]
-    end
+  def reviewer_location_link
+    @fields[:reviewer_location_link] = @reviewer_location["href"] unless @first_review
   end
 
-  def reviewer_name(item)
-    unless first_review_link(item)
-      review = item.at_css("div.listing_summary_body_content_review")
-      review.at_css("span.reviewer_location").remove
-      review.at_css("span.reviewer_name").text
-    end
+  def reviewer_name
+    @fields[:reviewer_name] = @review.at_css("span.reviewer_name").remove.text unless @first_review
   end
 
-  def review_text(item)
-    unless first_review_link(item)
-      review = item.at_css("div.listing_summary_body_content_review")
-      review.at_css("span.reviewer_location").remove
-      review.at_css("span.reviewer_name").remove
-      review.text.split(/\s+/).reject(&:empty?).join(" ").chomp(' -')
-    end
+  def review_text
+    reviewer_name unless fields[:reviewer_name]
+    @fields[:review_text] = @review.text.split(/\s+/).reject(&:empty?).join(" ").chomp(' -')
   end
 
-  def rating(item)
-    item.css('img[src$="star_gold.png"]').count
+  def rating
+    @fields[:rating] = @item.css('img[src$="star_gold.png"]').count
   end
 
-  def absolutize_links(fields)
-    fields.keys.select { |k| k =~ /link/ }.map! { |k| fields[k] = base + fields[k] if fields[k].start_with?('/') }
+  def absolutize_links
+    @fields.keys.select { |k| k =~ /link/ }.map! { |k| fields[k] = @base + fields[k] if fields[k].start_with?('/') }
   end
 
-  (1..249).each { |page| queue << page }
-  25.times do
-    threads << Thread.new do
-      until queue.empty? do
-        page = queue.pop(true) rescue nil
-        Thread::exit() if page.nil?
-        puts "Parsing page #{page}..."
-        doc = Nokogiri::HTML(open(base+url+page.to_s))
-        doc.css("div.listing_summary").each do |item|
-          fields = {}
-          restaurants << fields
-        end
-      end
-    end
-  end
-  threads.each(&:join)
-  Restaurant.delete_all
-  Restaurant.create restaurants
 end
 
 
